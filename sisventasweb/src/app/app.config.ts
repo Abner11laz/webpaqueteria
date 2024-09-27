3a8b0b54ceddd18599e3f6cbaaa768547711b7a8
Abner11laz <aruanol@miumg.edu.gt>
2024-09-26 21:36:50 -0600

    Validaciones del formulario clientes

--- sisventasweb/src/app/app.config.ts
+++ sisventasweb/src/app/app.config.ts
@@ -4,10 +4,12 @@ import { importProvidersFrom } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';  // Importar FormsModule
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
+    importProvidersFrom(CommonModule, FormsModule),  // Incluir FormsModule
     importProvidersFrom(CommonModule),
     provideRouter(routes), provideClientHydration()
   ]
--- sisventasweb/src/app/crear-cliente/crear-cliente.component.ts
+++ sisventasweb/src/app/crear-cliente/crear-cliente.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-crear-cliente',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './crear-cliente.component.html',
   styleUrl: './crear-cliente.component.css'
 })
 export class CrearClienteComponent {
+  cliente = {
+    codigo: '',
+    nombres: '',
+    apellidos: '',
+    nit: '',
+    direccion: '',
+    categoria: '',
+    estado: '1'
+  };
 
+  onSubmit(form: any) {
+    if (form.valid) {
+      console.log('Formulario válido', this.cliente);
+      // Aquí puedes agregar la lógica para enviar los datos o hacer otra acción
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 }
