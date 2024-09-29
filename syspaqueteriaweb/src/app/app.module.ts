5eb3d919a7aee86a2e690fb1a840848bc617eace
Abner11laz <aruanol@miumg.edu.gt>
2024-09-29 12:00:59 -0600

    Creacion de componentes
    pages
    services
    shared

--- syspaqueteriaweb/src/app/app.module.ts
+++ syspaqueteriaweb/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
+import { NopageFoundComponent } from './nopage-found/nopage-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopageFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    PagesModule
   ],
   providers: [
     provideClientHydration()
