78763734737ac85bc5a55d92d17f98a464747f75
Abner11laz <aruanol@miumg.edu.gt>
2024-10-10 23:22:57 -0600

    vista de clientes

--- syspaqueteriaweb/src/app/pages/customers/components/home/home.component.ts
+++ syspaqueteriaweb/src/app/pages/customers/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BreadcrumbsService } from '../../../../services/breadcrumbs.service';
+import { CustomerService } from '../../../../services/customer.service';
 
 @Component({
   selector: 'app-home',
@@ -7,18 +8,19 @@ import { BreadcrumbsService } from '../../../../services/breadcrumbs.service';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit{
+  clientes:any[]=[];
 
-  constructor(private breadCrumbService: BreadcrumbsService){}
+  constructor(private breadCrumbService: BreadcrumbsService, private customerService:CustomerService){}
   ngOnInit(): void {
     this.breadCrumbService.setBreadcrumbVisibility(true);
+    this.customerService.getCustomers().subscribe((response)=>{
+      this.clientes = response;
+    },
+  (error)=>{
+    console.log("error: ", error);
+  });
   }
 
-  
-
-  clientes=[
-    { id: 1, name: 'Juan Perez', email: 'juanperez@example.com', phone: '555-1234', invoices: 5, openOrders: 2, cancellations: 1 },
-    { id: 2, name: 'Maria Gomez', email: 'mariagomez@example.com', phone: '555-5678', invoices: 10, openOrders: 3, cancellations: 0 }
-  ];
 
   selectedCustomer: any = null;
   asideVisible = false;
--- syspaqueteriaweb/src/app/services/customer.service.ts
+++ syspaqueteriaweb/src/app/services/customer.service.ts
@@ -10,6 +10,10 @@ export class CustomerService {
   private baseUriApi = 'http://54.227.145.10/api/cliente'
   constructor(private httpClient: HttpClient) { }
 
+  getCustomers():Observable<any>{
+    return this.httpClient.get(`${this.baseUriApi}/listar-todo`);
+  }
+
   setCustomer(customerData:any):Observable<any>{
     return this.httpClient.post(`${this.baseUriApi}/crud`,customerData);
 
